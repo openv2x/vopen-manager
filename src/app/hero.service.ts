c92dfdbf160ff416353c1e9185071e59fb798165
Petar Vorotnikov <petar@vorotnikov.net>
2017-08-29 10:16:48 +0300

    Completed tutorial5

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero'
+import { HEROES } from  './mock-heroes';
 
 @Injectable()
 export class HeroService {
+    getHeroes(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
+    }
 
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve(HEROES), 5000);
+        });
+    }
 }
